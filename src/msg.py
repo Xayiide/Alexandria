25e5bb080dffa62b98c26658db64ba50f6a9cf82
Yakul <xayide.rg@gmail.com>
2019-11-17 15:00:40 +0100

    Moved echo() from msg.py to utils.py, I think it makes more sense

--- src/msg.py
+++ src/msg.py
@@ -1,12 +1,10 @@
-import utils
-import telegram
 
 ###############
 # Bot Messages
 ###############
 infoStartBot = """ Start message info """
 
-
+infoHelpBot = """ Help with Bot commands """
 
 
 
@@ -18,23 +16,4 @@ infoStartBot = """ Start message info """
 #                             #
 # Bot initialization message  #
 #                             #
-bot_init = "[+] Bot initialized"
-
-
-
-
-#      #
-# echo #
-#      #
-def echo(update):
-    chat_id = update.message.chat_id
-    name    = update.message.from_user.first_name
-    msg     = update.message.text
-
-    if utils.hasNick(update):
-        nick = update.message.from_user.username
-        print("\n\nMessage from chat: " + str(chat_id) + " [" + name + "] @" +
-               nick + "\n\t> " + str(msg))
-    else:
-        print("\n\nMessage from chat: " + str(chat_id) + " [" + name +
-              "] NONICK" + "\n\t> " + str(msg))
+log_init = "[+] Bot initialized"
