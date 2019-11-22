52b88d657a4e57fd1100e8a02cc0f75085d13100
Yakul <xayide.rg@gmail.com>
2019-11-22 01:17:35 +0100

    Added functions to Utils class. Set class to save configuration parameters between objects

--- src/utils.py
+++ src/utils.py
@@ -5,54 +5,47 @@ import telegram
 
 class Utils:
 
+    admins = []
+    token  = ""
+    dbhost = ""
+    dbuser = ""
+    dbpass = ""
+    db     = ""
+
     def __init__(self):
         pass
     
-    def readConfig(self, path):
-        pass
+    def readConfig(self):
+        config = configparser.ConfigParser()
+        config.read('../config.cfg')
+
+        self.token  = config['BOT']['token']
+        self.dbhost = config['DATABASE']['host']
+        self.dbuser = config['DATABASE']['user']
+        self.dbpass = config['DATABASE']['pass']
+        self.db     = config['DATABASE']['db']
+        
+        for admin in config['BOT']['admins'].split(','):
+            self.admins.append(int(admin))
     
     def hasNick(self, update):
-        pass
+        if update.message.from_user.username != None:
+            return True
+        return False
 
-    def echo(self, update):
-        pass
+    def echo(self, update): # TODO: Write to log instead
+        chat_id = update.message.chat_id
+        name    = update.message.from_user.first_name
+        msg     = update.message.text
 
+        if self.hasNick(update):
+            nick = update.message.from_user.username
+            print("\n\nMessage from chat: " + str(chat_id) + " [" + name +
+                    "] @" + nick + "\n\t> " + str(msg))
+        else:
+            print("\n\nMessage from chat: " + str(chat_id) + " [" + name +
+                    "] NONICK" + "\n\t> " + str(msg))
 
+    def isAdmin(self, user):
+        return user in self.admins
 
-def readConfig():
-    ret_   = {}
-    config = configparser.ConfigParser()
-    config.read('../config.cfg')
-
-    ret_["token"]  = config['BOT']['token']
-    ret_["dbhost"] = config['DATABASE']['host']
-    ret_["dbuser"] = config['DATABASE']['user']
-    ret_["dbpass"] = config['DATABASE']['pass']
-    ret_["db"]     = config['DATABASE']['db']
-
-    ret["admins"] = []
-    for i in config['BOT']['admins'].split(','):
-        ret["admins"].append(int(i))
-
-    return ret_
-
-def hasNick(update):
-    if update.message.from_user.username != None:
-        return True
-    return False
-
-# Print the update in the bot's console.
-# TODO: Write to log instead
-#
-def echo(update):
-    chat_id = update.message.chat_id
-    name    = update.message.from_user.first_name
-    msg     = update.message.text
-
-    if hasNick(update):
-        nick = update.message.from_user.username
-        print("\n\nMessage from chat: " + str(chat_id) + " [" + name + "] @" +
-               nick + "\n\t> " + str(msg))
-    else:
-        print("\n\nMessage from chat: " + str(chat_id) + " [" + name +
-              "] NONICK" + "\n\t> " + str(msg))
