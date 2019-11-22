8cde6ee99abcacefadb1053a45bbdbc0544151d8
Yakul <xayide.rg@gmail.com>
2019-11-22 01:18:11 +0100

    Added addCategory and rmCategory commands

--- src/commands.py
+++ src/commands.py
@@ -1,12 +1,11 @@
 import utils
-
+import msg
 
 # Commands
 def start(update, context):
     """ Send welcome message.
         Let know how to start the bot. """
-    utils.echo(update)
-    # TODO: Make it print what the bot's purpose is and how to start using it
+    # TODO: Make it print what the bot's purpose is and how to start using
     update.message.reply_text(msg.infoStartBot)
 
 
@@ -16,6 +15,29 @@ def help(update, context):
     utils.echo(update)
     # TODO: Add all commands
     update.message.reply_text(msg.infoHelpBot)
+
+
+def addCategory(update, context):
+    """ Add a category into the database """
+    utl = utils.Utils()
+    if not utl.isAdmin(update.message.from_user.id):
+        return 1
+    # 2. Now the user is an admin. Read the category sent
+    try:
+        categoryName = context.args[0]
+    except (IndexError, ValueError):
+        return 2
+    
+def rmCategory(update, context):
+    """ Remove a category from the database """
+    utl = utils.Utils()
+    if not utl.isAdmin(update.message.from_user.id):
+        return 1
+
+    try:
+        categoryName = context.args[0]
+    except (IndexError, ValueError):
+        return 2
 
 
 def addFromFile(bot, update):
