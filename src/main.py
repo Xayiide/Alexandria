88dc5111c5760c1d77d714345a98ea41a980fd8c
Yakul <xayide.rg@gmail.com>
2019-11-22 01:18:41 +0100

    Added usage of utils object and database object

--- src/main.py
+++ src/main.py
@@ -12,18 +12,24 @@ import commands  as cmds
 def main():
     print(msg.log_init)
 
-    cfg = utils.readConfig()
-    db_ = database.db(cfg['dbhost'], cfg['dbuser'],
-                      cfg['dbpass'], cfg['db'])
+    cfg = utils.Utils()
+    cfg.readConfig()
 
+
+    db_ = database.db(cfg.dbhost, cfg.dbuser, cfg.dbpass, cfg.db)
+    db_.connect()
 
     # Telegram-bot initialization
-    updater    = Updater(token=cfg['token'], workers=200, use_context=True)
+    updater    = Updater(token=cfg.token, workers=200, use_context=True)
     dp         = updater.dispatcher
 
     # Command handlers
     dp.add_handler(CommandHandler('start', cmds.start))
     dp.add_handler(CommandHandler('help', cmds.help))
+    dp.add_handler(CommandHandler('addCategory', cmds.addCategory,
+                                  pass_args=True))
+    dp.add_handler(CommandHandler('rmCategory', cmds.rmCategory,
+                                  pass_args=True))
 
     # Message Handlers
 
