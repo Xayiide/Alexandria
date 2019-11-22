a416a8e3bc87db4beeb850cb37e3bf838ed1c469
Yakul <xayide.rg@gmail.com>
2019-11-22 01:19:38 +0100

    Created functions and refactored everything to be more OOP

--- src/database.py
+++ src/database.py
@@ -1,29 +1,39 @@
-import pymysql
+import mysql.connector as sql
 
 class db:
+    
+    host     = ""
+    user     = ""
+    passwd   = ""
+    database = ""
+    conn     = None
+    cur      = None
+
     def __init__(self, host, user, passwd, database):
         self.host     = host
         self.user     = user
         self.passwd   = passwd
         self.database = database
         self.conn     = None
-        self.cur      = None
+        # self.cur      = None # We'll have one cursor per query
 
     def connect(self):
-        self.conn = pymysql.connect(self.host, self.user,
-                                    self.passwd, self.database)
-        self.cur  = self.conn.cursor()
+        self.conn = sql.connect(host=self.host,
+                                user=self.user,
+                                password=self.passwd,
+                                database=self.database)
         print("Connected to database")
+
+    def createTable(self, tableName):
+        cursor = sql.cursor()
+        try:
+            cursor.execute("CREATE TABLE {}".format(tableName))
+        except mysql.connector.Error as err:
+            print("Failed creating database: {}".format(err))
+            return 1
 
     def disconnect(self):
         self.conn.close()
-
-    def fetch(self, query):
-        self.__connect()
-        self.cur.execute(query)
-        res = self.cur.fetchall()
-        self.__disconnect()
-        return res
 
     def execute(self, query):
         self.__connect()
