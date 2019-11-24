9e5dd656f1c611be1725435f1784be42372c63dc
Yakul <xayide.rg@gmail.com>
2019-11-24 18:47:48 +0100

    Added commands to handle resources, categories and topics

--- src/commands.py
+++ src/commands.py
@@ -13,13 +13,11 @@ class Commands:
         self.utils    = utils
         self.database = database
 
-
     def start(self, update, context):
         """ Send welcome message.
             Let know how to start the bot. """
         print("Here we go starting")
         update.message.reply_text(msg.infoStartBot)
-
 
     def help(self, update, context):
         """ Send the list of commands used """
@@ -89,6 +87,40 @@ class Commands:
         except Exception as e:
             return 2
 
+
+    def addResource(self, update, context):
+        """ Add a resource to a specific category
+            and topic
+            /addResource [URL] [catName] [topicName] """
+        if not self.utils.isAdmin(update.message.from_user.id):
+            return 1
+        if (len(context.args) != 3):
+            print("[!!] Formato incorrecto - addResource")
+            return 1
+        try:
+            resourceURL  = context.args[0]
+            categoryName = context.args[1]
+            topicName    = context.args[2]
+            self.database.addResource(resourceURL, categoryName, topicName)
+        except Exception as e:
+            return 2
+
+    def rmResource(self, update, context):
+        """ Remove a resource from a specific category
+            and topic
+            /rmResource [URL] [catName] [topicName] """
+        if not self.utils.isAdmin(update.message.from_user.id):
+            return 1
+        if (len(context.args) != 3):
+            print("[!!] Formato incorrecto - rmResource")
+            return 1
+        try:
+            resourceURL  = context.args[0]
+            categoryName = context.args[1]
+            topicName    = context.args[2]
+            self.database.rmResource(resourceURL, categoryName, topicName)
+        except Exception as e:
+            return 2
 
     def addFromFile(self, update, context):
         pass
