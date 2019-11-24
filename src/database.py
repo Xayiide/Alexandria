97365a2736b6e0fcba4a5657be12a5f34f28e47a
Yakul <xayide.rg@gmail.com>
2019-11-24 15:22:15 +0100

    Added the database handlers for all four commands: add and remove categories and topics

--- src/database.py
+++ src/database.py
@@ -1,7 +1,8 @@
-import mysql.connector as sql
+import mysql.connector as sql_
+import queries as q
 
 class db:
-    
+
     host     = ""
     user     = ""
     passwd   = ""
@@ -18,19 +19,74 @@ class db:
         # self.cur      = None # We'll have one cursor per query
 
     def connect(self):
-        self.conn = sql.connect(host=self.host,
-                                user=self.user,
-                                password=self.passwd,
-                                database=self.database)
+        self.conn = sql_.connect(host=self.host,
+                                 user=self.user,
+                                 password=self.passwd,
+                                 database=self.database)
         print("Connected to database")
 
-    def createTable(self, tableName):
-        cursor = sql.cursor()
+    def addCatToTopic(self, catName, topicName):
+        cursor = self.conn.cursor()
         try:
-            cursor.execute("CREATE TABLE {}".format(tableName))
-        except mysql.connector.Error as err:
-            print("Failed creating database: {}".format(err))
+            # Retrieve the topicId for topicName
+            cursor.execute(q.selectFromWhere.format('topicId',
+                                                    'Topics',
+                                                    'topicName',
+                                                    '"' + topicName + '"'))
+            # fetchall will be like [(x,)] where x is the topicID
+            topicId = cursor.fetchall()[0][0] # Fetch only x, the topicID
+
+            cursor.execute(q.insertIntoCateg.format('"' + catName + '"',
+                                                   topicId))
+            self.conn.commit()
+        except Exception as e:
+            print("Error at addCatToTopic: {}".format(e))
             return 1
+        finally:
+            cursor.close()
+
+    def rmCatFromTopic(self, catName, topicName):
+        cursor = self.conn.cursor()
+        try:
+            # Retrieve the topic Id for the topicName
+            cursor.execute(q.selectFromWhere.format('topicId',
+                                                    'Topics',
+                                                    'topicName',
+                                                    '"' + topicName + '"'))
+            topicId = cursor.fetchall()[0][0]
+
+            cursor.execute(q.deleteFromCateg.format('"' + catName + '"',
+                                                    topicId))
+            self.conn.commit()
+        except Exception as e:
+            print("Error at rmCatFromTopic: {}".format(e))
+            return 1
+        finally:
+            cursor.close()
+
+
+    def addTopic(self, topicName):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(q.insertIntoTopics.format('"' + topicName + '"'))
+            self.conn.commit()
+        except Exception as e:
+            print("Error at addTopic: {}".format(e))
+            return 1
+        finally:
+            cursor.close()
+
+    def rmTopic(self, topicName):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(q.deleteFromTopics.format('"' + topicName + '"'))
+            self.conn.commit()
+        except Exception as e:
+            print("Error at rmTopic: {}".format(e))
+            return 1
+        finally:
+            cursor.close()
+
 
     def disconnect(self):
         self.conn.close()
